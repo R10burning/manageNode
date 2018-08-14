13e2e1d8bc8745a9d86c9deba8012f46e0741bdf
R10 <790309793@qq.com>
2018-08-14 23:33:59 +0800

    supervisor

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+// 全局安装npm i supervisor -g ====>supervisor server.js
 const express = require('express')
 
 const app = express()
 
 app.get('/', function (req, res) {
-	res.send('Hello World!')
+	res.send('Hello Worssssssssld!')
 })
 
 app.listen(3000, function () {
