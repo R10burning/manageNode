4bf9dc39f0f8d016bee6b6991305b2581d6b30e5
dengxiang <790309793@qq.com>
2018-08-27 17:29:12 +0800

    菜单数据

--- server.js
+++ server.js
@@ -75,6 +75,41 @@ app.get('/list', (req, res) => {
 		})
 	})
 })
+/* 菜单 */
+app.get('/menu', (req, res) => {
+	const menuData = [{
+		'id': 1,
+		'parent_id': 0,
+		'menu_name': '第一级菜单 1',
+		'sorting': 0,
+		'node': [{
+			'id': 2,
+			'parent_id': 1,
+			'menu_name': '第二级菜单 1-1',
+			'sorting': 0,
+			'node': [{
+				'id': 3,
+				'parent_id': 2,
+				'menu_name': '第三级菜单 1-1-1',
+				'sorting': 1
+			}]
+		}]
+	},
+	{
+		'id': 4,
+		'parent_id': 0,
+		'menu_name': '第一级菜单 2',
+		'sorting': 0,
+		'node': [{
+			'id': 5,
+			'parent_id': 4,
+			'menu_name': '第二级菜单 2-1',
+			'sorting': 0
+		}]
+	}
+	]
+	res.json(menuData)
+})
 
 app.get('/', (req, res) => {
 	res.send('hello')
